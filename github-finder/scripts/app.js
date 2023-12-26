ae8256dc857a55222c8d1fe20946de9ec096aab3
Juwwan <juwan0226@gmail.com>
2023-12-26 10:21:03 +0900

    github finder app 코딩 완료

--- github-finder/scripts/app.js
+++ github-finder/scripts/app.js
@@ -11,6 +11,7 @@ searchUser.addEventListener("keyup", (e) => {
       console.log(data);
       if (data.profile.message === "Not Found") {
         ui.showAlert("User not found", "alert alert-danger");
+        
       } else {
         ui.showProfile(data.profile);
         ui.showRepos(data.repos);
