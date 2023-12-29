3dd53b83ee996ab244abd7a04ed0e23ee14d86ff
ju-waaan <juwan0226@gmail.com>
2023-12-29 20:39:41 +0900

    SpreadSheet App 코딩 완료

--- spreadsheet-app/script.js
+++ spreadsheet-app/script.js
@@ -1,4 +1,5 @@
 const spreadSheetContainer = document.querySelector("#spreadsheet-container");
+const exportBtn = document.querySelector("#export-btn");
 const ROWS = 10;
 const COLS = 10;
 const spreadsheet = [];
@@ -17,6 +18,28 @@ class Cell {
         this.columnName = columnName;
         this.active = active;
     }
+}
+
+exportBtn.onclick = function (e) {
+    let csv = "";
+    for(let i = 0; i < spreadsheet.length; i++) {
+        if (i === 0) continue;
+        csv +=
+            spreadsheet[i]
+                .filter(item => !item.isHeader)
+                .map(item => item.data)
+                .join(',') + "\r\n";
+    }
+    console.log('csv: ', csv);
+
+    const csvObj = new Blob([csv]);
+    const csvUrl = URL.createObjectURL(csvObj);
+    console.log('csvUrl', csvUrl);
+
+    const a = document.createElement("a");
+    a.href = csvUrl;
+    a.download = 'spreadsheet name.csv';
+    a.click();
 }
 
 initSpreadsheet();
@@ -55,7 +78,7 @@ function initSpreadsheet() {
         spreadsheet.push(spreadsheetRow);
     }
     drawSheet();
-    console.log(spreadsheet);
+    // console.log(spreadsheet);
 }
 
 
@@ -71,19 +94,34 @@ function createCellEl(cell) {
     }
 
     cellEl.onclick = () => handleCellClick(cell);
+    cellEl.onchange = (e) => handleOnChange(e.target.value, cell);
 
     return cellEl;
 }
 
+function handleOnChange(data, cell) {
+    cell.data = data;
+}
+
 function handleCellClick(cell) {
+    clearHeaderActiveStates();
     const columnHeader = spreadsheet[0][cell.column];
     const rowHeader = spreadsheet[cell.row][0];
     const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
     const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
     columnHeaderEl.classList.add('active');
     rowHeaderEl.classList.add('active');
-    console.log('clicked cell', columnHeader, rowHeader);
+    // console.log('clicked cell', columnHeaderEl, rowHeaderEl);
+    document.querySelector("#cell-status").innerHTML = cell.columnName + cell.rowName;
 }
+
+function clearHeaderActiveStates() {
+    const headers = document.querySelectorAll('.header');
+
+    headers.forEach((header) => {
+        header.classList.remove('active');
+    })
+} 
 
 function getElFromRowCol(row, col) {
     return document.querySelector("#cell_" + row + col);
